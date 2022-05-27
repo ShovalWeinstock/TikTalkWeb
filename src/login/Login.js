6e56cda3f1e2323fb3222bc1950588c78c6cbf35
shoval <shoval.weinstock@live.biu.ac.il>
2022-05-27 19:22:13 +0300

    added reviews page

--- src/login/Login.js
+++ src/login/Login.js
@@ -2,7 +2,7 @@ import '../LoginAndRegister.css';
 import { useState } from 'react';
 import { login } from './loginValidations.js';
 
-function Login({changeRegState, changeLoggedInUser}) {
+function Login({ changeRegState, changeLoggedInUser }) {
 
     // typed username and password
     const [username, setUsername] = useState('');
@@ -14,7 +14,7 @@ function Login({changeRegState, changeLoggedInUser}) {
     }
 
     // submit the form
-    async function handleSubmit(e){
+    async function handleSubmit(e) {
         clearErrors();
         e.preventDefault();
         var user = await login(username, password);
@@ -25,32 +25,37 @@ function Login({changeRegState, changeLoggedInUser}) {
     }
 
     return (
-        <div className="main-block" id="login">
-            <h1>Login</h1>
-            <form onSubmit={handleSubmit}>
-                {/*input field for username*/}
-                <div className="mb-3">
-                    <label htmlFor="inputUsername2" className="form-label">Username</label>
-                    <input type="username" className="form-control" id="inputUsername2" aria-describedby="usernameHelp"
-                        value={username} onChange={(e) => setUsername(e.target.value)}></input>
-                </div>
-                {/*input field for password*/}
-                <div className="mb-3">
-                    <label htmlFor="inputPassword2" className="form-label">Password</label>
-                    <input type="password" id="inputPassword2" className="form-control" aria-describedby="passwordHelpBlock"
-                        value={password} onChange={(e) => setPassword(e.target.value)}></input>
-                </div>
-                {/*submit button*/}
-                <button type="Login" className="btn btn-primary">Submit</button>
-                {/*registeration for new users*/}
-                <span id="notRegistered" className="form-text"> Not registered?</span>
-                <span>
-                    <a onClick={changeRegState} className="form-text"> Click here</a>
-                </span>
-                <span className="form-text"> to regiser</span>
-                <p id="loginError" className="errorMessege"></p>
-            </form>
+        <div>
+            <div className="main-block" id="login">
+                <h1>Login</h1>
+                <form onSubmit={handleSubmit}>
+                    {/*input field for username*/}
+                    <div className="mb-3">
+                        <label htmlFor="inputUsername2" className="form-label">Username</label>
+                        <input type="username" className="form-control" id="inputUsername2" aria-describedby="usernameHelp"
+                            value={username} onChange={(e) => setUsername(e.target.value)}></input>
+                    </div>
+                    {/*input field for password*/}
+                    <div className="mb-3">
+                        <label htmlFor="inputPassword2" className="form-label">Password</label>
+                        <input type="password" id="inputPassword2" className="form-control" aria-describedby="passwordHelpBlock"
+                            value={password} onChange={(e) => setPassword(e.target.value)}></input>
+                    </div>
+                    {/*submit button*/}
+                    <button type="Login" className="btn btn-primary">Submit</button>
+                    {/*registeration for new users*/}
+                    <span id="notRegistered" className="form-text"> Not registered?</span>
+                    <span>
+                        <a onClick={changeRegState} className="form-text"> Click here</a>
+                    </span>
+                    <span className="form-text"> to regiser</span>
+                    <p id="loginError" className="errorMessege"></p>
+                </form>
+            </div>
+
+            <a href="http://localhost:5102/" id="rateUs" className="btn btn-primary">Rate Us!</a>
         </div>
+
     );
 }
 
